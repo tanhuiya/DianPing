ecd3f89e6535d6a992db261bdf2065ae47108740
tanhuiya <403554355@qq.com>
2015-12-11 21:31:34 +0800

    Initial Commit

---
+++ DianPing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DianPing
+//
+//  Created by tanhui on 15/12/11.
+//  Copyright © 2015年 tanhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
